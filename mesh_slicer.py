d81f3cd4e1a40c21f5e2c41dbf1e90e016e6b765
Jonathan Otten <cubemail134@gmail.com>
2026-06-15 12:30:28 +0200

    did some tuning

--- mesh_slicer.py
+++ mesh_slicer.py
@@ -79,8 +79,8 @@ class Slicer:
         self.point_spacing = 0.1
         self.z_sample_height = 0.03
         self.z_detection_distance = self.layer_height
-        self.horizontal_detection_distance = 0.5 * self.nozzle_diameter
-        self.min_line_segments = 5
+        self.horizontal_detection_distance = 0.8 * self.nozzle_diameter
+        self.min_line_segments = 20
 
     def slice(self) -> List[List[Point]]:
         """
